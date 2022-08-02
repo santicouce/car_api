48ef8d72f8bbdabdb1c55ef45bbe94e9be31dfcc
Santiago <santicouce@outlook.com>
2022-08-02 13:18:27 -0300

    move cred to env file and set up database url for mysql

--- API/database.py
+++ API/database.py
@@ -1,9 +1,13 @@
-from venv import create
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
 
-SQL_DATABASE = "sqlite:///./todos.db"
-engine = create_engine(SQL_DATABASE, connect_args={"check_same_thread": False})
+mysql_user = os.getenv("MYSQL_USER")
+mysql_password = os.getenv("MYSQL_PASSWORD")
+mysql_host = os.getenv("MYSQL_HOST")
+mysql_database = os.getenv("MYSQL_DATABASE")
+SQL_DATABASE = f"mysql+pymysql://{mysql_user}:{mysql_password}@{mysql_host}/{mysql_database}"
+engine = create_engine(SQL_DATABASE)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
