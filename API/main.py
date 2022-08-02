7d019277e01fc6d5f536e954d7029b707c30bc8c
Santiago <santicouce@outlook.com>
2022-08-02 13:18:51 -0300

    add auth router and load env file

--- API/main.py
+++ API/main.py
@@ -1,8 +1,12 @@
+from dotenv import load_dotenv
+
+load_dotenv("env/test.env")
 from fastapi import FastAPI
-from routers import cars, drivers
+from routers import cars, drivers, auth
+
 
 app = FastAPI()
 
 app.include_router(cars.router)
 app.include_router(drivers.router)
-
+app.include_router(auth.router)
